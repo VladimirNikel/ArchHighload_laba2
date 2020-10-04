75ed093261f0265bda6c7d9dcc528e64040f7459
VladimirNikel <vova_niemisto@mail.ru>
2020-10-04 15:24:12 +0300

    Добавлено ограничение по памяти, подправлен запуск с изоляцией пространства имем

--- second_part.c
+++ second_part.c
@@ -1,4 +1,8 @@
 #define _GNU_SOURCE
+#define CLONE_NEWNS
+#define CLONE_NEWPID
+#define CLONE_NEWNET
+#define SIGCHLD
 #include <unistd.h>								//sethostname()
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,9 +11,11 @@
 #include <time.h>								//rand()
 #include <sys/wait.h>							//sleep()
 #include <sys/utsname.h>						//struct utsname
+#include <string.h>
 
-static char child_stack[1048576];
-char str[201]="";
+
+char str[1001]="";								//объявление переменной, в которой будет храниться команда и ее аргументы
+int flags = CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWNET | SIGCHLD;
 
 void print_nodename(){
 	struct utsname utsname;
@@ -20,11 +26,11 @@ void print_nodename(){
 int child_fn(){
 	printf("\nПервоначальное имя узла:");
 	print_nodename();
-	//printf("Имя будет изменено в новом пространстве имён!\n");
 	//генерация рандомного имени - числа
-	char newNodename[10];
+	char newNodename[24];
 	sprintf(newNodename, "%d", rand());
-	sethostname(newNodename, 11);				//NedoDocker 11
+	strcat(newNodename, "-NedoDocker");
+	sethostname(newNodename, 23);				//NedoDocker 11
 
 	printf("Новое имя узла: ");
 	print_nodename();
@@ -35,22 +41,84 @@ int child_fn(){
 
 
 	//выполнение введеной программы
-	printf("\nВывод программы\n=================================================\n");
-	if(system(str) != 0)
-		printf("\n=================================================\n\tПрограмма \"упала\". А я вырубаюсь.");
-	else
-		printf("\n=================================================\nПрограмма корректно завершилась - не паримся и не тратим ресурсы компьютера. Я спать.");
-	printf("\n\n\tСпасибо, что пользуетесь этим недоDocker'ом.\nNikel (c)\n");
+	printf("\nВывод программы\n==================================================\n");
+	if(system(str) != 0)											//выполнение введеной команды, и если возвращаемое значение не 0 - орём, паникуем, плачем
+		printf("\n==================================================\n\tПрограмма \"упала\". Ну и я вырубаюсь.");
+	else															//иначе - всё ок, идем довольными спать
+		printf("\n==================================================\nПрограмма корректно завершилась. Я сделал всё что мог и пошёл довольным спать.");
+	printf("\n\n%s\n%50s\n","     Спасибо, что пользуетесь этим недоDocker'ом.", "Nikel (c), 2020");
 	return 0;
+}
+
+pid_t create_clone(void **stack, int kb){
+	int stack_size = kb * 1024;										//сколько килобайт выделим процессу (для его стека)
+	*stack = malloc(stack_size);
+	void *stack_top = (char *) *stack + stack_size;					//
+	return clone(child_fn, stack_top, flags, NULL);					//создание клона процесса с изоляцией в соотвествии с флагами
 }
 
 int main(int argc, char **argv){
 	srand(time(NULL));
-	printf("Введите относительный путь запускаемой команды (вместе с параметрами): ");
-	gets(str);
-	pid_t child_pid = clone(child_fn, child_stack+1048576, CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWNET | SIGCHLD, NULL);
-	sleep(1);
+	printf("**************************************************\nАвтор: Ниемисто Владимир, М3О-117М-20\nНазвание программы: \"Жалкое подобие Docker'а\"\nОписание: Эта вундер-вафля умеет запускать изолировано процессы, с возможностью ограничивать процесс по доступной ему памяти.\n**************************************************\n\n");
+
+	printf("1. Введите относительный путь запускаемой команды (вместе с параметрами): ");
+	//gets(str);					//путь запускаемой команды
+	fgets(str, 1000, stdin);		//путь запускаемой команды
+	
+	int kb = 0;
+	while(kb <= 0){
+		printf("2. Введите размер передаваемого стека (в килобайтах): ");
+		scanf("%d", &kb);
+	}
+	
+	/*
+	printf("\n\nПо умолчанию используется изоляция по:\n\t1. Пространству имён\n\t2. PID'ам процессов\n\t3. Пространству имён NET\n\t4. Файловой системе\n");
+	char result[5]="";
+	printf("\nЖелаете внести изменения (отключив какую-нибудь изоляцию)? (Y/n): ");
+	//fgets(result, 4, stdin);
+	scanf("%2s", result);
+	if((int)result[1] == (char)'Y' || (int)result[1] == (char)'y' ){					// Y- 89, y - 121
+		printf("Хорошо, какую изоляцию отключить?\n\t1. Пространства имён\n\t2. PID'ам процессов\n\t3. Пространства имён NET\n\t4. Файловой системе\n\nВведите номер отключаемой изоляции: ");
+		int isolation_off = 0;
+		scanf("%d", &isolation_off);
+		switch(isolation_off){
+			case 1:
+				flags = CLONE_NEWPID | CLONE_NEWNET | SIGCHLD;
+				printf("\tВы отключили изоляцию пространства имён.\n");
+				break;
+			case 2:
+				flags = CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWNET | SIGCHLD;
+				printf("\tВы отключили изоляцию процессов.\n");
+				break;
+			case 3:
+				flags = CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWPID | SIGCHLD;
+				printf("\tВы отключили изоляцию сетей (имён NET).\n");
+				break;
+			case 4:
+				flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNET | SIGCHLD;
+				printf("\tВы отключили изоляцию файловой системы.\n");
+				break;
+			default:
+				printf("\tВы ввели неверные данные - я оставляю всё как было.");
+				flags = CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWNET | SIGCHLD;//флаги
+				break;
+		}
+	}
+	else{
+		printf("результат: %s\n", result);
+		printf("1 = %d\t2 = %d\ty = %d, Y = %d", result[1], result[2], (char)'y', (char)'Y');
+		printf("\n1 int = %d, 1 char = %d\n",(int)result[1], (char)result[1]);
+	}
+	*/
+	
+	void *stack;
+	pid_t child_pid = create_clone(&stack, kb);
+	//sleep(1);
 	waitpid(child_pid, NULL, 0);
+	free(stack);
 	return 0;
 }
+
+
+
 
